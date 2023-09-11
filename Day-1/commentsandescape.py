3d7d72a1d98aa1e47d848da71a5d6d384e888f4e
Bonikaadhikari <adhikaribonika@gmail.com>
2023-09-11 14:55:23 +0545

    Poem, Comments, Escape character Learnt

---
+++ Day-1/commentsandescape.py
@@ -0,0 +1,17 @@
+print("Bonika \t Adhikari")
+# This is my name 
+
+print("Hello World")
+'''This program is saying
+  Hello World to all the 
+  People'''
+
+#Escape Character
+print("He asked, \"Your grandparents are still alive? Aren\'t they?\" ")
+print("0 \t 0 \t 1 \n1 \t 0 \t 1 \n0 \t 1 \t 0")
+
+#Determinant of matrix
+print("3 \t2 \n2 \t1 \n= (3*1) - (2*2)")
+print("= 3 - 4 \n= -1")
+print("Since, determinant of the given matrix isn\'t equals to 0. The matrix isn\'t singular.")
+
--- Day-1/index.py
+++
@@ -1,4 +0,0 @@
-if 2>5:
-	print("5 is greater")
-else:
-	print("2 is greater")
