fd3270e7ac01033ed71c6d068076aad8da91c3f1
Bonikaadhikari <adhikaribonika@gmail.com>
2023-09-12 14:55:15 +0545

    Variables, Datatypes and Operators Learnt

---
+++ Day-1/Operators.py
@@ -0,0 +1,25 @@
+a = 10
+b = 5
+
+ans1 = (a + b) 
+print("Addition of", a, "and", b, "is", ans1)
+
+ans2 = (a - b)
+print("Subtraction of", a, "and", b, "is", ans2)
+
+ans3 = (a * b)
+print("Multiplication of", a, "and", b, "is", ans3)
+
+ans4 = (a / b)
+print("Division of", a, "and", b, "is", ans4)
+
+ans5 = (a % b)
+print("Modulus of", a, "and", b, "is", ans5)
+
+ans6 = (a // b)
+print("FloorDivision of", a, "and", b, "is", ans6)
+#FloorDivision print the value without decimal value
+
+ans7 = (a ** b)
+print("Exponential of", a, "times", b, "is", ans7)
+# ** helps to give power of some numbers with other
---
+++ Day-1/VaribaleAndDataType.py
@@ -0,0 +1,17 @@
+#Variables
+a = 5
+b="Bonika"
+c=None 
+print("The type of b is", type(b))
+print("The type of c is", type(c))
+
+#List
+list1 = [(2, 4, 3), ["Apple", "Banana", "Mango"]]
+print(list1)
+
+#Tuple
+
+
+#Dictonary
+dict1 = {"name":"Bonika", "Age":"21", "CanVote":"True"}
+print(dict1)
