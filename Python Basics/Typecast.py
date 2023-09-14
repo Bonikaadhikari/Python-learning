e040bef75ba67190a44caf6e33f85a7e01202695
Bonikaadhikari <adhikaribonika@gmail.com>
2023-09-14 16:21:40 +0545

    Learnt Typecasting

---
+++ Python Basics/Typecast.py
@@ -0,0 +1,14 @@
+a = 1
+b = "4" #String
+
+print( a + int(b)) #int helps to convert string b to integer
+
+#Typecasting means converting one datatype into another
+
+#Implicit TypeCasting
+
+x = 6
+y = 7.5
+
+z = x + y
+print("The type of", z, "is", type(z))
