ed3226a9a2e30db1a4b29ae3cf983b97bdecb156
Bonikaadhikari <adhikaribonika@gmail.com>
2023-09-15 17:08:38 +0545

    Used operators by taking input from user

---
+++ Python Basics/SimpleCalculator.py
@@ -0,0 +1,20 @@
+a = int(input("Enter a number"))
+b = int(input("Enter another number"))
+
+ans1 = a + b
+print("The sum of", a, "and", b, "is", ans1)
+
+ans2 = a - b
+print("The subtraction of", a, "and", b, "is", ans2)
+
+ans3 = a * b
+print("The multiplication of", a, "and", b, "is", ans3)
+
+ans4 = a / b
+print("The division of", a, "by", b, "is", ans4)
+
+ans5 = a % b
+print("The modolus of", a, "and", b, "is", ans5)
+
+
+
