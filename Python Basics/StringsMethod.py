fe4a17a4b5e4b46f181d1e172d7f9017ee7a2a91
Bonikaadhikari <adhikaribonika@gmail.com>
2023-09-17 16:19:25 +0545

    MultiLilne strings and indexing learnt

---
+++ Python Basics/StringsMethod.py
@@ -0,0 +1,6 @@
+a = '''Hi, My name is Bonika Adhikari. I am here today on day-11 of python programming.
+I am enjoying a lot in this programming language.'''
+print(a)
+
+print(a[0:8]) #Index helps to print the requried string with the help of their index number
+print(a[:])
