61780ec1489694cd569e0d04bb4b99a93e9f26e5
Bonikaadhikari <adhikaribonika@gmail.com>
2023-09-20 16:49:10 +0545

    Positive and Negative slicing of string learnt

---
+++ Python Basics/StringSlicing.py
@@ -0,0 +1,10 @@
+n = "I am interested in programming"
+print(n[0])
+print(n[2:6])
+print("The length of", n, "is", len(n) ) #len() helps to find out the length of Strings. It counts space also.
+fruits = "Apple, Mango, Orange"
+print(fruits[0:5]) #It prints the index including 0 but not 5
+print(fruits[2:-4]) #It prints the string from two to the total length -4
+
+
+
