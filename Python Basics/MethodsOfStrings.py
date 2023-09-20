22f6f738ac32baf8a2ba86345ab91ee43c1faa86
Bonikaadhikari <adhikaribonika@gmail.com>
2023-09-20 17:12:01 +0545

    Some strings methods learnt

---
+++ Python Basics/MethodsOfStrings.py
@@ -0,0 +1,14 @@
+a = "My name is Bonika Adhikari"
+print(a)
+print(a.upper())
+print(a.lower())
+print(a.replace("My name is","I am"))
+
+b = "Hi!"
+print(b.strip("!"))
+
+animal = "cat dog cow"
+print(animal.split(" "))
+
+c = "bonika"
+print(c.capitalize())
