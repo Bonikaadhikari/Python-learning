75eb47bedba5567469f6716deccb6ad2048649ca
Bonikaadhikari <adhikaribonika@gmail.com>
2023-09-22 18:36:16 +0545

    If, IfElse and ElIf Statement learnt

---
+++ Python Basics/ConditionalOperators.py
@@ -0,0 +1,38 @@
+a = int(input("Enter Your Age: "))
+print("Your age is ", a)
+
+# print(a == 15) #is euqal to
+# print(a<15) #is less than
+# print(a>15) #is greater than
+# print(a!=15) #is not equal to
+# print(a>=15) #greater than and equal to
+
+
+# if(a>=18):
+#     print("You can vote")
+# else:
+#     print("You cannot vote")
+
+if(a>=60):
+    print("Your are Senior Citizen")
+elif(a>=20):
+    print("You are Young")
+elif(a>=12):
+    print("You are TeenAger")
+else:
+    print("You are a Child")
+
+n = int(input("Enter a value of number"))
+if(n>0):
+    print("The number is positive")
+elif(n==0):
+    print("The number is Zero")
+else:
+    print("The number is zero")
+
+
+
+
+
+
+
