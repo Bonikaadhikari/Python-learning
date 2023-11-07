c5fc6ee2d7e8c0d1b8af1dd18e33f8d36afe07d6
Bonikaadhikari <adhikaribonika@gmail.com>
2023-11-07 18:10:28 +0545

    While Loop Learnt

--- Revision/Day6.py
+++ Revision/Day6.py
@@ -1,20 +1,25 @@
 #This is my Day-6 of revision in basics of Python for PhiCoders. 
 #Today i will go with for loop.
 
-#For Loop
-name = "Bonika"
-for i in name:
-    print(i, end=',') 
+# #For Loop
+# name = "Bonika"
+# for i in name:
+#     print(i, end=',') 
 
-colors = {"Red", "White", "Green", "Blue"}
-for i in colors:
-    print(i, end = ',') 
+# colors = {"Red", "White", "Green", "Blue"}
+# for i in colors:
+#     print(i, end = ',') 
 
-for n in range(20):
-    print(n+1)
+# for n in range(20):
+#     print(n+1)
 
-for i in range(10,20):
+# for i in range(10,20):
+#     print(i)
+
+# for k in range(1, 20, 2):
+#     print(k)
+
+i = 0
+while(i>5):
     print(i)
-
-for k in range(1, 20, 2):
-    print(k)
+    i = i + 1
