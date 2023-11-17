0ceb82ac40c776c4048752898ad7dbe4171f9f8b
Bonikaadhikari <adhikaribonika@gmail.com>
2023-11-17 18:18:50 +0545

    Learnt to Raise a custom Error

--- Revision/Day15.py
+++ Revision/Day15.py
@@ -1,5 +1,5 @@
 #This is my Day-15 Revision in basics of Python In PhiCoders
-#Today I will go for methods of Dictonaries.
+#Today I will go for methods of Dictonaries and Raise a custom error.
 
 #Update
 dic = {1: "Bonika", 2: "Shrutee", 3: "Ram"}
@@ -31,3 +31,12 @@ print(dic)
 dic = {1: "Bonika", 2: "Shrutee", 3: "Ram"}
 a = dic.get(2)
 print(a)
+
+#Custom Error
+age = int(input("Enter a positive number"))
+if(age<=0):
+    raise ValueError("Please provide a positive number")
+elif(age>=16):
+    print("You can Vote")
+else:
+    print("You are Non-Voter")
