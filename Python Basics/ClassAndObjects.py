4873dc504364a89d0af6081d58207e90b637feff
Bonikaadhikari <adhikaribonika@gmail.com>
2023-12-02 15:38:41 +0545

    Classes and Objects Learnt with Self Keyword

--- Python Basics/ClassAndObjects.py
+++ Python Basics/ClassAndObjects.py
@@ -7,3 +7,22 @@ print(a.name, a.address)
 
 a.name= "Shrutee"
 print(a.name)
+
+class Staffs:
+    name = "Ram"
+    age = "30"
+    post = "Teacher"
+    salary = "35000"
+    def info(self):
+        print(f"{self.name} is a {self.post}. He is {self.age} years old. His monthly income is {self.salary}")
+
+x = Staffs()
+y = Staffs()
+x.name = "Shyam"
+x.age = 32
+
+y.name = "Hari"
+x.post = "Principal"
+
+x.info()
+y.info()
